ff5bd1f9485f2617390a7e8a4e3ca63ed16683ea
JAY00012 <jayspacestar24@gmail.com>
2024-09-09 16:03:51 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- for icons  -->
+    <link rel="stylesheet" href="css/material-icons.css">
+    <!-- for animation  -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- for slider  -->
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="style.css">
+     <!-- Bootstrap Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap Link -->
+
+
+    <!-- Font Awesome Cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <!-- Font Awesome Cdn -->
+
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <!-- Google Fonts -->
+
+    </head>
+    
+
+
+<?php include 'menu.php';   ?>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/show.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Necklace</h3>
+        <p>"Where Timeless Beauty Comes Alive"</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/earing5.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Earring</h3>
+        <p>"Don't Just Wear Earrings, Flaunt Them with Catchy Confidence"</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/bracelet2.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Bracelet</h3>
+        <p>"Unique Bracelets for Unique Souls"</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div> 
+
+<section class="packages" id="packages">
+      <div class="container">
+        
+        <div class="main-txt">
+          <h1><span>P</span>roducts</h1>
+        </div>
+
+        <div class="row" style="margin-top: 30px;">
+
+          <div class="col-md-4 py-3 py-md-0">
+
+            <div class="card">
+              <img src="images/R1.jpg" alt="Ring">
+              <div class="card-body">
+                <h3>Contemporary Charms Platinum Ring</h3>
+                <p>Make your mark everyday when you adorn this contemporary ring crafted in 18 karat rose gold, studded with diamonds Sparkling Contemporary.</p>
+                <div class="star">
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                </div>
+                <h6>Price: <strong>$50000</strong></h6>
+                <a href="contacts.php">Buy Now</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-4 py-1 py-md-0">
+
+            <div class="card">
+              <img src="images/n2.jpg" alt="Necklace">
+              <div class="card-body">
+                <h3>Basic Charms Diamond Necklace Set</h3>
+                <p>Simplicity and grace come together in this classy stylish fusion 18 karat rose gold necklace set studded with breathtaking diamonds. Stone Clarity SI2</p>
+                <div class="star">
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star "></i>
+                </div>
+                <h6>Price: <strong>$530000</strong></h6>
+                <a href="contacts.php">Buy Now</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+
+            <div class="card">
+              <img src="images/28.jpg" alt="">
+              <div class="card-body">
+                <h3>Delicate Detail Pure Gold stud Bangle </h3>
+                <p>Embrace the day with this airy, filigree cut Bangle crafted with 22 Karat yellow gold, come together in this classy stylish fusion ,offering a touch of sophistication.</p>
+                <div class="star">
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star "></i>
+                </div>
+                <h6>Price: <strong>$60000</strong></h6>
+                <a href="contacts.php">Buy Now</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="row" style="margin-top: 30px;">
+
+          <div class="col-md-4 py-3 py-md-0">
+
+            <div class="card">
+              <img src="images/earring1.jpeg" alt="Earring">
+              <div class="card-body">
+                <h3>Contemporary  Stud Earrings</h3>
+                <p>Sparkling Contemporary 18 Karat Gold and Diamond  polish finish. Stone clarity: SI2</p>
+                <div class="star">
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                </div>
+                <h6>Price: <strong>$50067</strong></h6>
+                <a href="contacts.php">Buy Now</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-4 py-4 py-md-0">
+
+            <div class="card">
+              <img src="images/B2.jpg" alt="">
+              <div class="card-body">
+                <h3>Brilliant Geometric Bracelet</h3>
+                <p>Bring a touch of brilliance to your style with this geometric laser cut bracelet crafted in 22 karat yellow gold</p>
+                <div class="star">
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star "></i>
+                  <i class="fa-solid fa-star "></i>
+                </div>
+                <h6>Price: <strong>$78560</strong></h6>
+                <a href="contacts.php">Buy Now</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+
+            <div class="card">
+              <img src="images/goldcoin.jpeg" alt="Gold coin">
+              <div class="card-body">
+                <h3>Goddess Lakshmi  22 Karat Gold  </h3>
+                <p>This 8 gram coin is a symbol of purity and prosperity, crafted in 22 karat yellow gold.</p>
+                <div class="star">
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star "></i>
+                </div>
+                <h6>Price: <strong>$6660</strong></h6>
+                <a href="contacts.php">Buy Now</a>
+              </div>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+      </div>
+    </section>
+    <!-- Section Packages End -->
+ </div>
+        <a href="productcart.php" id="viewmorebtn">View More</a>
+    </div>
+
+
+    <h2 class="text-center">STORE PHOTO GALLERY</h2>
+    <p>&nbsp;</p>
+      <p>&nbsp;</p></th>
+    <td width="934"><div align="center"><img src="images/gift1.gif" width="900" height="420" /></div></td>
+  </tr>
+  </div>
+
+ <p>&nbsp;</p>
+      <p>&nbsp;</p></th>
+ 
+
+
+    <!-- product cards -->
+<div class="container">
+    <div class="row ">
+        <div class="col-xl-6 col-lg-6">
+            <div class="card l-bg-cherry">
+                <div class="card-statistic-3 p-4">
+                    <div class="card-icon card-icon-large"><i class="fas fa-shopping-cart"></i></div>
+                    <div class="mb-4">
+                        <h5 class="card-title mb-0">New Orders</h5>
+                    </div>
+                    <div class="row align-items-center mb-2 d-flex">
+                        <div class="col-8">
+                            <h2 class="d-flex align-items-center mb-0">
+                                3,243
+                            </h2>
+                        </div>
+                        <div class="col-4 text-right">
+                            <span>12.5% <i class="fa fa-arrow-up"></i></span>
+                        </div>
+                    </div>
+                    <div class="progress mt-1 " data-height="8" style="height: 8px;">
+                        <div class="progress-bar l-bg-cyan" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6 col-lg-6">
+            <div class="card l-bg-blue-dark">
+                <div class="card-statistic-3 p-4">
+                    <div class="card-icon card-icon-large"><i class="fas fa-users"></i></div>
+                    <div class="mb-4">
+                        <h5 class="card-title mb-0">Customers</h5>
+                    </div>
+                    <div class="row align-items-center mb-2 d-flex">
+                        <div class="col-8">
+                            <h2 class="d-flex align-items-center mb-0">
+                                15.07k
+                            </h2>
+                        </div>
+                        <div class="col-4 text-right">
+                            <span>9.23% <i class="fa fa-arrow-up"></i></span>
+                        </div>
+                    </div>
+                    <div class="progress mt-1 " data-height="8" style="height: 8px;">
+                        <div class="progress-bar l-bg-green" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6 col-lg-6">
+            <div class="card l-bg-green-dark">
+                <div class="card-statistic-3 p-4">
+                    <div class="card-icon card-icon-large"><i class="fas fa-ticket-alt"></i></div>
+                    <div class="mb-4">
+                        <h5 class="card-title mb-0">Ticket Resolved</h5>
+                    </div>
+                    <div class="row align-items-center mb-2 d-flex">
+                        <div class="col-8">
+                            <h2 class="d-flex align-items-center mb-0">
+                                578
+                            </h2>
+                        </div>
+                        <div class="col-4 text-right">
+                            <span>10% <i class="fa fa-arrow-up"></i></span>
+                        </div>
+                    </div>
+                    <div class="progress mt-1 " data-height="8" style="height: 8px;">
+                        <div class="progress-bar l-bg-orange" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6 col-lg-6">
+            <div class="card l-bg-orange-dark">
+                <div class="card-statistic-3 p-4">
+                    <div class="card-icon card-icon-large"><i class="fas fa-dollar-sign"></i></div>
+                    <div class="mb-4">
+                        <h5 class="card-title mb-0">Revenue Today</h5>
+                    </div>
+                    <div class="row align-items-center mb-2 d-flex">
+                        <div class="col-8">
+                            <h2 class="d-flex align-items-center mb-0">
+                                $11.61k
+                            </h2>
+                        </div>
+                        <div class="col-4 text-right">
+                            <span>2.5% <i class="fa fa-arrow-up"></i></span>
+                        </div>
+                    </div>
+                    <div class="progress mt-1 " data-height="8" style="height: 8px;">
+                        <div class="progress-bar l-bg-cyan" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+   <p>&nbsp;</p>
+      <p>&nbsp;</p></th>
+  <!-- Footer Start -->
+    <footer id="footer">
+      <h1><span>J</span>WELLERY</h1>
+      <p>“Jewellery is like the perfect spice – it always complements what's already there.” “Jewellery is eternal, it never goes out of fashion, it will always be there.” “Into every girl's life, a little diamond should fall.” “I favour pearls on screen and in my private life.”</p>
+      <div class="credit">
+        <p>Designed By <a href="#">JW</a></p>
+      </div>
+      <div class="copyright">
+        <p>&copy;Copyright JW Coding. All Rights Reserved</p>
+        <p class="py-3 bg-dark text-white text-center">
+  @jaywandhekarProduction</p>
+    </div>
+    </footer>
+    <!-- Footer End -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+    </html>
